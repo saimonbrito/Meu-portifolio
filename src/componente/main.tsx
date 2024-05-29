2ff2b3f9273e4dd61e11b711f7eee5586f61c796
mauricio fernandes <britomauricio89@gmail.com>
2024-05-29 11:53:46 -0300

    novas atualizações redes sociais

--- src/componente/main.tsx
+++ src/componente/main.tsx
@@ -1,17 +1,27 @@
 import Fotocapa from '../../public/img/foto.jpeg'
+import In from '../../public/img/linkedin .png'
+import Git from '../../public/img/github.png'
+import zap from '../../public/img/whatsapp (1).png'
+
 
 export function Main(){
     return(
-        <div className="w-5/6 h-2/2 p-10 rounded-2xl bg-yellow-50 shadow-xl md:back 
-            sm:bg-slate-500
+        <div className="w-screen h-auto p-10   
+            sm:
             md:bg-red-500
             lg:bg-amber-500
             xl:bg-lime-500
             2xl:bg-slate-900
             ">
                   {/* conteudo do corpo */}
-                <div className='flex fle justify-center items-center'> 
-                    <div className='flex-1 text-green-900 text-sm text-1xl '>
+                <div className='flex fle justify-between items-center'> 
+                    <div className='flex flex-col gap-4'>
+                       <strong><img src={In} width={40} alt="" /></strong>
+                       <strong><img src={Git} width={40} alt="" /></strong>
+                       <strong><img src={zap} width={40} alt="" /></strong>
+                       <strong>iconi4</strong>
+                    </div>
+                    <div className=' text-green-900 text-sm text-1xl '>
                     
                     <p>Com determinação e paixão pela tecnologia <br/>
                       estou pronto para mostrar meu potencial <br/>
