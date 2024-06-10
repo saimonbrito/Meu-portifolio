98c535ba783832a64c86ffdea26ad249fec05fa6
mauricio fernandes <britomauricio89@gmail.com>
2024-06-10 09:23:00 -0300

    criando link para rede sociais

--- src/componente/main.tsx
+++ src/componente/main.tsx
@@ -16,10 +16,10 @@ export function Main(){
                   {/* conteudo do corpo */}
                 <div className='flex fle justify-between items-center'> 
                     <div className='flex flex-col gap-4'>
-                       <strong><img src={In} width={40} alt="" /></strong>
-                       <strong><img src={Git} width={40} alt="" /></strong>
-                       <strong><img src={zap} width={40} alt="" /></strong>
-                       <strong>iconi4</strong>
+                       <strong><a href="https://www.linkedin.com/in/mauricio-fernandes-8393071a6/" ><img src={In} width={40} alt="" /></a></strong>
+                       <strong><a href="https://github.com/saimonbrito"><img src={Git} width={40} alt="" /> </a></strong>
+                       <strong><a href="*"><img src={zap} width={40} alt="" /></a></strong>
+                       <strong></strong>
                     </div>
                     <div className=' text-green-900 text-sm text-1xl '>
                     
